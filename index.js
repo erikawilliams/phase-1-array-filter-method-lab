1b649f11f17461d7cb5b1db08485c9037596336a
Erika Williams <erika@erikawilliams.us>
2022-03-16 11:29:36 -0600

    commit tree c236540b0c19ec6df874197d31c2e86627d0240b

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching (array, name){
+    return array.filter(element => element.toLowerCase() === name.toLowerCase())
+    }
+
+function fuzzyMatch (array,name){
+    return array.filter(function(element){
+        return element.slice(0,name.length).toLowerCase() === name.toLowerCase()
+    })
+}
+
+function matchName(array,name){
+    return array.filter(function(element){
+        return element["name"] === name;
+    })
+}
